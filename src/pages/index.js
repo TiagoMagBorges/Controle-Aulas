9e7f5da690f8b43c8c87aa24a1ba7d429ce6c64f
TiagoMagBorges <tiagomagborges@gmail.com>
2024-10-27 22:15:20 -0300

    feat(index): starting to build the landpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,70 @@
+import Link from "next/link";
+
 export default function Home() {
     return (
-        <>
+        <div style={{
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            background: '#0F0F0F',
+            flexDirection: 'column',
+        }}>
             <nav style={{
-                width: '100vw',
-                height: '7vh',
-                position: 'fixed',
-                top: '0%',
-                backgroundColor: 'black',
-                color: 'white',}}>
-                <h1>Navbar</h1>
-            </nav>
-            <main style={{
-                width: '100vw',
-                height: '100vh',
-                paddingTop: '7vh',
+                width: '100%',
+                height: '10%',
+                display: 'flex',
+                alignItems: 'center',
+                margin: 'auto',
+                flexDirection: 'row',
                 color: 'white',
-                backgroundColor: '#123524',
             }}>
-                <h1>Main Content</h1>
+
+                <div style={{
+                    width: '10%',
+                    height: '100%',
+                    alignItems: 'center',
+                    alignContent: 'center',
+                    justifyContent: 'center',
+                    display: 'flex',
+                    fontWeight: 'bold',
+                    fontSize: '25px',
+                    textShadow: '0 0 10px #CCC, 0 0 20px #CCC, 0 0 30px #CCC, 0 0 40px #CCC, 0 0 50px #CCC',
+                }}>SEP
+                </div>
+
+                <div style={{
+                    width: '10%',
+                    height: '100%',
+                    alignItems: 'center',
+                    alignContent: 'center',
+                    justifyContent: 'center',
+                    display: 'flex',
+                    marginLeft: 'auto',
+                }}>
+                    <Link href="/login" style={{
+                        textDecoration: 'none',
+                        color: 'inherit',
+                    }}>Login / Criar Conta</Link>
+                </div>
+
+            </nav>
+
+            <main style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                width: '99%',
+                height: '90%',
+                padding: '20px',
+                backgroundColor: '#FFF',
+                margin: 'auto',
+                borderRadius: '20px',
+            }}>
+                Teste
             </main>
-            <footer>
-                <h1>Footer</h1>
-            </footer>
-        </>
+        </div>
     );
 }
